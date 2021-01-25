69eaaebfa2bbb84a1feb8dd517af647c485b4a4e
BlasDams <samassaadama@hotmail.fr>
2021-01-25 19:29:37 +0100

    exo 1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css';
+const tempMin = -20;
+const tempMax = 20;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0;
+const stepsMax = 50000;
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
