01e17142b8c544556da9495049f48ba7e3432015
BlasDams <samassaadama@hotmail.fr>
2021-01-28 14:52:17 +0100

    fin jour1, debut part2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import './App.css';
-import Steps from './components/Steps/Steps'
+import Box from './components/Box/Box'
+import './styles/global.css'
 const tempMin = -20;
-const tempMax = 20;
+const tempMax = 40;
 const heartMin = 80;
 const heartMax = 180;
 const stepsMin = 0;
 const stepsMax = 50000;
+
 export class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      water: 0,
+      heart: 120,
+      temperature: -10,
+      steps: 3000
+    }
+  }
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-            <p>heartMin : {heartMin}</p>
-            <p>tempMin : {tempMin}</p>
-            <p>stepsMin : {stepsMin}</p>
-            <Steps/>
+            <p>Heart : {heartMin}</p>
+            <p>Temperature : {tempMin}</p>
+            <p>Steps : {stepsMin}</p>
+        </div>
+        <div className="row">
+          <Box icon={"local_drink"} color={"#3A85FF"} value={1.5} unit={"L"}/>
+          <Box icon={"directions_walk"} color={"black"} value={3000} unit={"steps"}/>
+          <Box icon={"favorite"} color={"red"} value={120} unit={"bpm"}/>
+          <Box icon={"wb_sunny"} color={"yellow"} value={-10} unit={"°C"}/>
         </div>
       </div>
     );
